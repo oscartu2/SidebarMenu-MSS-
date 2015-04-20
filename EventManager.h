a7965561140a6279d0c9694286f60bfb79280ad7
oscartu2 <o.car.w2@gmail.com>
2015-04-20 09:59:35 -0700

    open source not even mine hueheuehu

---
+++ EventManager.h
@@ -0,0 +1,13 @@
+//
+//  EventManager.h
+//  SidebarMenu
+//
+//  Created by Oscar Tu on 2015-04-20.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventManager : NSObject
+
+@end
---
+++ EventManager.m
@@ -0,0 +1,13 @@
+//
+//  EventManager.m
+//  SidebarMenu
+//
+//  Created by Oscar Tu on 2015-04-20.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+#import "EventManager.h"
+
+@implementation EventManager
+
+@end
